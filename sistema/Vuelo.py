3bd6ce4ff078b74aa7b01986f0c589ae297d9154
Ami2610 <162734950+Ami2610@users.noreply.github.com>
2025-08-11 23:39:15 -0500

    actualización

--- sistema/Vuelo.py
+++
@@ -1,9 +0,0 @@
-class Vuelo:
-    def __init__(self, origen, destino, fecha, avion):
-        self.origen = origen              # string, ej: "Madrid"
-        self.destino = destino            # string, ej: "París"
-        self.fecha = fecha                # string, ej: "01/09/2025"
-        self.avion = avion                # objeto Avion
-
-    def __str__(self):
-        return f"{self.origen} → {self.destino} ({self.fecha}) - {self.avion.modelo})"
--- sistema/main.py
+++
@@ -1,4 +0,0 @@
-from InterfazAerolinea import InterfazAerolinea
-
-if __name__ == "__main__":
-    InterfazAerolinea()
